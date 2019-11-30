a227ad6090feb363c724834d208fded49ec944b7
John Linatoc <johnlinatoc1227@gmail.com>
2019-11-30 10:00:26 -0600

    Create initializer with hungry? and feed methods

--- dog_spec.rb
+++ dog_spec.rb
@@ -1,11 +1,31 @@
 require 'rspec'
 require_relative 'dog'
 
-describe Dog do
+RSpec.describe Dog do
   describe '#bark' do
     it 'returns the string "Woof!"' do
-      dog = Dog.new
-      expect(dog.bark).to eql('Woof!')
+      expect(subject.bark).to eql('Woof!')
+    end
+  end
+
+  describe '#feed' do
+    subject { described_class.new(hunger_level: 7) }
+
+    it 'is no longer hungry' do
+      subject.feed
+      expect(subject).to_not be_hungry
+    end
+  end
+
+  describe '#hungry?' do
+    it 'returns true if hunger_level is more than 5' do
+      dog = described_class.new(hunger_level: 7)
+      expect(dog).to be_hungry
+    end
+
+    it 'returns false if hunger_level is 5 or less' do
+      dog = described_class.new(hunger_level: 5)
+      expect(dog).to_not be_hungry
     end
   end
 end
