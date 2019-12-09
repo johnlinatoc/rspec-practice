3118bb1472065be1923a00ac19fc6238bdd32006
John Linatoc <johnlinatoc1227@gmail.com>
2019-12-08 20:08:33 -0600

    Add initialize method to Cat

--- cat.rb
+++ cat.rb
@@ -1,4 +1,11 @@
 class Cat
+
+  attr_reader :name
+
+  def initialize(name:)
+    @name = name
+  end
+
   def meow
     "Meow!"
   end
--- cat.spec.rb
+++ cat.spec.rb
@@ -2,9 +2,16 @@ require 'rspec'
 require_relative 'cat'
 
 describe Cat do
-  describe '#meow' do
-    subject {described_class.new}
+  subject {described_class.new(name: 'Max')}
 
+  describe '#initialize' do
+    it 'takes in a name as a string' do
+      name = 'Max'
+      expect(subject).to have_attributes(name: name)
+    end
+  end
+
+  describe '#meow' do
     it 'returns the string "Meow!"' do
       expect(subject.meow).to eq("Meow!")
     end
