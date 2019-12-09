f6819c4075800459f6ad3756e6980157ef189c87
John Linatoc <johnlinatoc1227@gmail.com>
2019-12-08 22:02:57 -0600

    Start ask_name method in command_line_interface

--- command_line_interface.spec.rb
+++ command_line_interface.spec.rb
@@ -5,5 +5,10 @@ describe Command_line_interface do
   subject { described_class.new }
 
   context '#ask_name' do
+    it 'takes in name and outputs it' do
+      expect(STDOUT).to receive(:puts).with("What's your name?")
+      subject.input.stub(:gets) { 'John' }
+      expect(subject.ask_name).to eq('Welcome John!')
+    end
   end
 end
